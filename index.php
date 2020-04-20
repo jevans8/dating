92dd7c0fe93b843c0aa7f9b7643fb8a58c6e5836
Julia Evans <jevans8@mail.greenriver.edu>
2020-04-20 19:16:28 -0400

    Added default route

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate the framework (Base class)
+$f3 = Base::instance(); //Class::method()
+
+//Define a default route
+$f3->route('GET /', function(){
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run the framework (fat free)
+$f3->run();
+
